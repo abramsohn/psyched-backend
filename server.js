0ce0a8722c8c10665ac40fc7f4e01488160ad1dd
Ido Abramsohn <ido.abramsohn@gmail.com>
2021-04-02 08:08:08 -0400

    initial commit with basic express setup

---
+++ server.js
@@ -0,0 +1,19 @@
+// DEPENDENCIES //
+const express = require('express');
+
+// CONFIGURATION //
+const app = express();
+
+// MIDDLEWARE
+
+// MODELS //
+
+// CONTROLLERS //
+
+// LISTENER //
+const PORT = 3000;
+
+
+app.listen(PORT, () => {
+    console.log('Express listening at', PORT);
+});
